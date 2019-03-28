7698e11f6923150c9d5710c4f417ba17a14c487e
mukesh-urbanpiper <mukesh@urbanpiper.com>
2019-03-28 18:37:57 +0530

    promise removed

--- index-module.js
+++ index-module.js
@@ -1,32 +1,33 @@
+let writeListener = null;
+
 const asyncStorage = {
 	cache:{
 
 	},
+	onWriteComplete: function(callback) {
+		console.log('Listening to write')
+		writeListener = callback
+	},
 	setItem: function(key, value) {
-        return new Promise((resolve, reject)=> {
+		if(!(key && value)) {
+			throw{
+				error:"Data not valid. Expected: (key, value)"
+			}
+		}
 
-            if(!(key && value)) {
-                throw{
-					error:"Data not valid. Expected: (key, value)"
-				}
-            }
-
-    		if(localStorage.getItem('__AS')){
-    			var __AS = JSON.parse(localStorage.getItem('__AS'));
-    			__AS[key] = value;
-    		}else{
-    			var __AS = {};
-    			__AS[key] = value;
-    		}
-    		asyncStorage.cache = __AS
-
-    		writeToDisk(__AS, resolve)
-        })
+		if(localStorage.getItem('__AS')){
+			var __AS = JSON.parse(localStorage.getItem('__AS'));
+			__AS[key] = value;
+		}else{
+			var __AS = {};
+			__AS[key] = value;
+		}
+		asyncStorage.cache = __AS
+		writeToDisk(__AS)
 	},
 	getItem: function(key) {
 
         if(asyncStorage.cache[key]) {
-			console.log('returning from in-mem cache')
 			return asyncStorage.cache[key];
 		}
 		if(localStorage.getItem('__AS')) {
@@ -57,29 +58,35 @@ const asyncStorage = {
 			localStorage.setItem('__AS',JSON.stringify(__AS));
 			asyncStorage.cache = __AS
 		}
+		return
 	},
 	flush: function() {
 		localStorage.removeItem('__AS');
+		return
 	}
 }
 
 
 
 let writingToDisk = false
-function writeToDisk(obj, resolve) {
+function writeToDisk(obj) {
 	const __OBJ = obj;
 
 	if(writingToDisk) {
-		console.log('debouncing..')
+		// console.log('debouncing..')
 	}else {
 		writingToDisk = true;
 		setTimeout(()=>{
-			console.log('writing to disk');
+			// console.log('writing to disk');
 			localStorage.setItem('__AS', JSON.stringify(__OBJ));
 			writingToDisk = false;
-			resolve(__OBJ)
+
+			if(writeListener) {
+				writeListener(__OBJ)
+			}
 		},500)
 	}
+	return
 }
 
 module.exports = asyncStorage;
